a628329ee93cf5e9d8d658ae9ebb6f634a660c49
damaiyanti1 <damaiyanti497@gmail.com>
2024-12-04 22:35:10 +0700

    Add files via upload

---
+++ retail_app/modul/pemasok/index.php
@@ -0,0 +1,23 @@
+<div class="border-bottom d-flex justify-content-between py-3">
+    <h4>Data Pemasok</h4>
+    <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="modalTambah"><i class="bi bi-plus"></i> Tambah Pemasok</button>
+    <!-- Modal -->
+<div class="modal fade" id="ModalTambah" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Tambah Data Pemasok</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
